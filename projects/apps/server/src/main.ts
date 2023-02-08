7a73241d1c698f410f001e374dff23783217d4f3
TREHOUT Nathan <ntrehout@outworld.fr>
2023-02-08 16:16:01 +0100

    feat(apps-server/main.ts): add a global prefix /api

--- projects/apps/server/src/main.ts
+++ projects/apps/server/src/main.ts
@@ -6,9 +6,11 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || 3333;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
 bootstrap();
