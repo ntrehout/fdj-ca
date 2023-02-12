c91287e221935b7b64dc3cec339d234bf0cc6778
TREHOUT Nathan <ntrehout@outworld.fr>
2023-02-12 16:37:16 +0100

    feat(app-server/schema): create team.schema.ts

---
+++ projects/apps/server/src/app/schemas/team.schema.ts
@@ -0,0 +1,40 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+import { ITeam } from '@fdj-ca/shared-models';
+
+export type TeamDocument = Team & Document;
+
+@Schema()
+export class Team implements ITeam {
+  @Prop({ required: true })
+  name: string;
+  @Prop({ required: false })
+  banner: string;
+  @Prop({ required: false })
+  descriptionEN: string;
+  @Prop({ required: false })
+  descriptionFR: string;
+  @Prop({ required: false })
+  facebook: string;
+  @Prop({ required: false })
+  instagram: string;
+  @Prop({ required: false, type: Object })
+  stadium: {
+    name: string;
+    location: string;
+    capacity: number;
+    thumbnail: string;
+  };
+  @Prop({ required: false })
+  thumbnail: string;
+  @Prop({ required: false })
+  twitter: string;
+  @Prop({ required: false })
+  website: string;
+  @Prop({ required: false })
+  youtube: string;
+  @Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: 'Player' })
+  players: string[];
+}
+
+export const TeamSchema = SchemaFactory.createForClass(Team);
