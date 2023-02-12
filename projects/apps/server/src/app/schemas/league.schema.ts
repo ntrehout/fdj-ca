e138453a89a7cb5ca90fbde051587ea98e951df2
TREHOUT Nathan <ntrehout@outworld.fr>
2023-02-12 16:37:08 +0100

    feat(app-server/schema): create league.schema.ts

---
+++ projects/apps/server/src/app/schemas/league.schema.ts
@@ -0,0 +1,16 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { ILeague as ILeague } from '@fdj-ca/shared-models';
+
+export type LeagueDocument = League & Document;
+
+@Schema()
+export class League implements ILeague {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  sport: string;
+}
+
+export const LeagueSchema = SchemaFactory.createForClass(League);
